93000ea2679a694755c3d7fd05a7bc8491b61a87
Yurii19870524 <116317088+Yurii19870524@users.noreply.github.com>
2023-04-18 15:01:05 +0000

    Deploying to gh-pages from @ Yurii19870524/goit-js-hw-09@927d809727b1bc20accd72f0acf13084480b0dc4 🚀

--- 01-color-switcher.22de2b17.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world");
-//# sourceMappingURL=01-color-switcher.22de2b17.js.map
--- 01-color-switcher.567e9d34.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world");
-//# sourceMappingURL=01-color-switcher.567e9d34.js.map
